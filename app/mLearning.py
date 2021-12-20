c6b0313b661725e4b949619d897c6c3321f0d600
sondangtesla <sondangtesla98@gmail.com>
2021-12-20 13:30:01 +0700

    cleaning code and add comment

--- app/mLearning.py
+++ app/mLearning.py
@@ -4,6 +4,79 @@ import os, os.path
 import pandas as pd
 import shutil
 import rasterio
+import gdal
+import glob
+from pydub import AudioSegment
+from pydub.utils import make_chunks
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
+global path4
+
+path4="G:/My Drive/"
+
+class Select_Dir:
+  """
+  function for generate a dialog for select folder
+  """
+  def folder(self):
+    win= Tk()
+    
+    # Define the geometry
+    win.geometry("250x250")
+    
+    # Create a label and a Button to Open the dialog
+    path= filedialog.askdirectory(title="Select a Folder")
+    
+    win.destroy()
+    return path
+
+
+class Tiling:
+  """
+  this function will generate tile of image, and store every images in one folder
+  input: path_in, path_out
+  all the outputs will be moved to google drive automatically, once the tiling process done
+  """
+  def make_tile(self,path_in, base_path):
+    #specify size of tiling
+    tile_size_x = 500
+    tile_size_y = 500
+    
+    #specify path of images we need to tile
+    in_path = path_in
+    baseDir = base_path
+    input_filename = os.listdir(in_path) # all filenames stored in variable
+
+    #this is a loop for tiling every image in one directory
+    for n in range(len(input_filename)):
+      base=os.path.basename(input_filename[n])
+      nama=os.path.splitext(base)[0]
+      path = in_path+ '/'+str(input_filename[n])
+      
+      ds = gdal.Open(path)
+      band = ds.GetRasterBand(1)
+      xsize=band.XSize
+      ysize=band.YSize
+
+      #creating directory before tiling
+      path1=baseDir+'/'+str(nama)
+      os.makedirs(path1)
+      print(path1)
+
+      #create tiling
+      for i in range(0, xsize, tile_size_x):
+        for j in range(0, ysize, tile_size_y):
+          com_string = "gdal_translate -of GTIFF -srcwin " + str(i) + ", " \
+                        + str(j) + ", " + str(tile_size_x) + ", " \
+                        + str(tile_size_y) + " " + str(in_path+'/')\
+                        + str(input_filename[n]) + " " + str(path1+'/')\
+                        + str(nama)+ "_" + str(i) + "_" + str(j) + ".tiff"
+          
+          os.system(com_string)
+          print(com_string)
+    print("moving to drive..")
+    shutil.move(baseDir, path4)
 
 
 class Model:
@@ -14,8 +87,6 @@ class Model:
     outputs = CSV of lat long of object detected for GIS shp file
 
     """
-    
-    
     print(filename) 
     print(image_path)
     
@@ -35,25 +106,33 @@ class Model:
         if ext.lower() not in valid_images:
             continue
         
-        
         imgs.append(Image.open(os.path.join(path,f))) #opens images 
         geos.append(rasterio.open(os.path.join(path,f))) # gets coordinates of tiff
     
     print("starting second for"+str(n))
     
-    for img,geo in zip(imgs,geos) : #iterate over two list/tuple in parallel
-      
-      results = model(img) #inputs image to model
-      
-      boredpile_ds = results.pandas().xyxy[0] #outputs a pandas df with cols of coordinates, class of object and confidence score
-    
-      # Below we reapply the coordinates extracted from the tif files to our xmin,xmax,ymin,ymax to convert to latlong
+    """
+    - iterate over two list/tuple in parallel
+    - inputs image to model
+    - outputs a pandas df with cols of coordinates, 
+      class of object and confidence score
+    - Below we reapply the coordinates extracted from the tif files 
+      to our xmin,xmax,ymin,ymax to convert to latlong
+    """
 
-      boredpile_ds["xmin"], boredpile_ds["ymin"] = geo.transform * (boredpile_ds.xmin,boredpile_ds.ymin) # transforms coordinates to geoloc
-      boredpile_ds["xmax"], boredpile_ds["ymax"] = geo.transform * (boredpile_ds.xmax, boredpile_ds.ymax) # same as above
-      boredpile_ds =boredpile_ds[['name','class','xmin', 'ymin', 'xmax','ymax','confidence']] # removes unneeded columns
+    for img,geo in zip(imgs,geos) : 
+      results = model(img) 
+      boredpile_ds = results.pandas().xyxy[0] 
+    
+      # transforms coordinates to geoloc
+      boredpile_ds["xmin"], boredpile_ds["ymin"] = geo.transform * (boredpile_ds.xmin,boredpile_ds.ymin) 
+      boredpile_ds["xmax"], boredpile_ds["ymax"] = geo.transform * (boredpile_ds.xmax, boredpile_ds.ymax)
+
+      # removes unneeded columns 
+      boredpile_ds =boredpile_ds[['name','class','xmin', 'ymin', 'xmax','ymax','confidence']] 
       
-      total.append(boredpile_ds) #we need to appends result of each object (here in pandas df) to a list so to get one giant df
+      #appends result of each object (here in pandas df) to a list so to get one giant df
+      total.append(boredpile_ds)
       
     print("starting concat"+str(n))
     total = pd.concat(total,ignore_index=True) #concatanate to list of results df to one big dataframe
--- app/views.py
+++ app/views.py
@@ -1,19 +1,24 @@
 from app import app
-from app import tiling as tile #calling file tiling.py from folder app as tile
 from app import mLearning as ml #calling file mLearning.py from folder app as ml
 from flask import render_template
 import os, os.path
 import torch
-
-# calling class from file .py and save it to a variable
-
-tiling = tile.Tiling() #calling class Tiling from tiling.py, and save it to a variable named tiling
-models = ml.Model() #calling class Model from mLearning.py, and save it to a variable named models
-directory = tile.select_dir() #calling class select_dir from tiling.py, and save it to a variable named directory
+global path4
+"""
+calling class from file .py and save it to a variable
+calling class Model from mLearning.py, and save it to a variable named models
+calling class select_dir from tiling.py, and save it to a variable named directory
+"""
+path4="G:/My Drive/"
+tiling = ml.Tiling() 
+models = ml.Model() 
+directory = ml.Select_Dir()
 
 @app.route("/home")
 def index():
     return render_template("index.html")
+
+
 
 """
 endpoint for image tiling
@@ -23,12 +28,12 @@ input: a folder contain files of WP
 def tiling_images():
     path_in=directory.folder()
     base_path=directory.folder()
-    if len(path_in)==0 and len(base_path)==0:
+    if len(path_in) == 0 and len(base_path) == 0:
         return"specify the input and output path correctly"
     else:
         path2 = os.listdir(path_in)
         last_path=os.path.basename(base_path)
-        if os.path.exists("G:/My Drive/"+last_path)==True:
+        if os.path.exists(path4 + last_path)==True:
             return"output folder you specified: "+str(base_path) +" already exist in google drive"
         else:
             for f in path2:
@@ -36,23 +41,26 @@ def tiling_images():
                     return"input directory must only contain .tiff image file"
                 else:
                     tiling.make_tile(path_in,base_path)
-                    return "the results is ready on G:/My Drive/"+str(last_path)
-    
+                    return "the results is ready on"+str(path4 + last_path)
+
+
+
 """
 endpoint for boredpile detection
 input: a folder contain tiling folders every WP
 """
+
 @app.route("/model")
 def model():
     #calling our model
-    model_path = "G:/My Drive/best (5).pt" #insert model file path located in drive
+    model_path = str(path4)+"best5.pt" #insert model file path located in drive
     model = torch.hub.load('ultralytics/yolov5', 'custom', model_path)  # default
     
     #define in directpries to get image_path and out directories to save the result
     path_in = directory.folder()
     path_out = directory.folder()
 
-    if len(path_in)==0 and len(path_out)==0:
+    if len(path_in) == 0 and len(path_out) == 0:
         return"specify the input and output path correctly"
     else:
         pathh=os.listdir(path_in)
@@ -71,7 +79,7 @@ endpoint for roof detection
 @app.route("/roof")
 def roof_model():
     #calling our model
-    model_path = "G:/My Drive/roof-weights/v4-90.pt" #insert model file path located in drive
+    model_path = str(path4)+"roof_weights/v4-90.pt" #insert model file path located in drive
     model = torch.hub.load('ultralytics/yolov5', 'custom', model_path)  # default
     
     
